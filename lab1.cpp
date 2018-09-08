0769fe3503588e7a917381cc1ddf420abeb001ca
Rodp63ucsp <42578792+Rodp63ucsp@users.noreply.github.com>
2018-09-07 23:05:37 -0500

    Add files via upload

---
+++ lab1.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+  /*
+  //Problema 1
+  double a,b,c;
+  cin>>a>>b>>c;
+  cout<<"La suma es "<<(a+b+c)<<endl;
+  cout<<"El promedio es "<<(a+b+c)/3<<endl;
+  if(a>=b && a>=c){
+    cout<<"El mayor es "<<a<<endl;
+  }
+  else{
+    if(b>=a && b>=c){
+      cout<<"El mayor es "<<b<<endl;
+    }
+    else{
+      cout<<"El mayor es "<<c<<endl;
+    }
+  }
+  if(a<=b && a<=c){
+    cout<<"El menor es "<<a<<endl;
+  }
+  else{
+    if(b<=a && b<=c){
+      cout<<"El menor es "<<b<<endl;
+    }
+    else{
+      cout<<"El menor es "<<c<<endl;
+    }
+  }
+
+  //Problema 2: par o impar
+  int n;
+  cin>>n;
+  if(n%2==0){
+    cout<<n<<" es par"<<endl;
+  }
+  else{
+    cout<<n<<" es impar"<<endl;
+  }
+
+  //Problema 3: 
+  int x,y;
+  cin>>x>>y;
+  if(x%y==0){
+    cout<<x<<" es multiplo de "<<y<<endl;
+  }
+  else{
+    cout<<x<<" no es multiplo de "<<y<<endl;
+  }
+
+  //Problema 4: escribir digitos
+  int n;
+  cin>>n;
+  for(int i=10000; i>0; i=i/10){
+    cout<<(n/i)<<"\t";
+    n = n%i;
+  }
+  cout<<endl; */
+
+  //Problema 5: Potencias
+  int e = 0;
+  cout<<"entero cuadrado cubo"<<endl;
+  cout<<e<<"\t"<<(e*e)<<"\t"<<(e*e*e)<<endl;
+  e++;
+  cout<<e<<"\t"<<(e*e)<<"\t"<<(e*e*e)<<endl;
+  e++;
+  cout<<e<<"\t"<<(e*e)<<"\t"<<(e*e*e)<<endl;
+  e++;
+  cout<<e<<"\t"<<(e*e)<<"\t"<<(e*e*e)<<endl;
+  e++;
+  cout<<e<<"\t"<<(e*e)<<"\t"<<(e*e*e)<<endl;
+  e++;
+  cout<<e<<"\t"<<(e*e)<<"\t"<<(e*e*e)<<endl;
+  e++;
+  cout<<e<<"\t"<<(e*e)<<"\t"<<(e*e*e)<<endl;
+  e++;
+  cout<<e<<"\t"<<(e*e)<<"\t"<<(e*e*e)<<endl;
+  e++;
+  cout<<e<<"\t"<<(e*e)<<"\t"<<(e*e*e)<<endl;
+  e++;
+  cout<<e<<"\t"<<(e*e)<<"\t"<<(e*e*e)<<endl;
+  e++;
+  
+  
+  
+}
+
+
